786241209d013ae0bf81ed26e89e6916bd6bba99
Kenneth M. Glassey <kenneth.glassey@therengroup.com>
2024-02-28 14:54:29 -0600

    Add future imports line per Ansible requirement.

--- tests/unit/plugins/modules/conftest.py
+++ tests/unit/plugins/modules/conftest.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 import pytest
 
 from ansible_collections.zabbix.zabbix.plugins.module_utils.zabbix_api import (ZabbixApi)
